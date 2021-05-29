ae16643d8e73935d4010ac20955b63b96cdc633c
Gabriel Rodrigues de Lira <grl@blue-ocean-robotics.com>
2021-05-30 01:47:18 +0200

    Changing reputation positive threshold; Changing initial reputation

--- Assets/Scripts/Reputation.cs
+++ Assets/Scripts/Reputation.cs
@@ -11,7 +11,7 @@ public class Reputation : MonoBehaviour
     [SerializeField]
     float multiplierFactor = 10f;
     [SerializeField]
-    float positiveReputationThreshold = 0.5f;
+    float positiveReputationThreshold = 0.314159265359f;
     float _reputation;
     public Text reputationText;
 
@@ -20,7 +20,7 @@ public class Reputation : MonoBehaviour
 
     void Start()
     {
-        _reputation = 0;
+        _reputation = 380;
     }
 
     public void UpdateReputation(GameObject gameObject){
